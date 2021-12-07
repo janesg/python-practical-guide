ba0e6b8665bef991b667ec9ec0ec3ef4aa071595
Gary Janes <gjanes@numerix.com>
2021-12-07 17:35:38 +0000

    Basic python operators

---
+++ basics.py
@@ -0,0 +1,9 @@
+
+print('10 > 8 : ' + str(10 > 8))
+print('10 < 8 : ' + str(10 < 8))
+print('15 / 10 : ' + str(15 / 10))
+print('15 // 10 : ' + str(15 // 10))
+print('2 ** 3 : ' + str(2 ** 3))
+print('16 % 5 : ' + str(16 % 5))
+print('\'Bye \' * 5 : ' + 'Bye ' * 5)
+print('"Bye " * 5 : ' + "Bye " * 5)
