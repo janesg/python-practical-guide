43c3dc67b56a6d15e9eaf2692df4d1d78520c916
Gary Janes <gary_janes@hotmail.com>
2021-12-07 17:55:51 +0000

    Longer split line example

--- basics.py
+++ basics.py
@@ -7,3 +7,7 @@ print('2 ** 3 : ' + str(2 ** 3))
 print('16 % 5 : ' + str(16 % 5))
 print('\'Bye \' * 5 : ' + 'Bye ' * 5)
 print('"Bye " * 5 : ' + "Bye " * 5)
+
+long_line = """A longer line 
+      spanning multiple lines"""
+print(long_line)
